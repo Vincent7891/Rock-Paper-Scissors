ac6e1e3cc801e381f7fa7e055bb1b08da12e085e
Vincent7891 <137709573+Vincent7891@users.noreply.github.com>
2023-08-12 16:28:52 +0100

    Add CSS styling and separate .js file

---
+++ script.js
@@ -0,0 +1,127 @@
+let wins = 0;
+let losses = 0;
+let draws = 0;
+    
+function getComputerChoice(){
+    const moves = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random()*moves.length);
+    return moves[randomIndex];
+}
+
+function playerSelection(){
+    const rock = document.getElementById("rock");
+    const paperButton = document.getElementById("paper");
+    const scissorsButton = document.getElementById("scissors");
+
+    rock.addEventListener("click", function() {
+        playOneRound("rock", getComputerChoice());
+    });
+
+    paper.addEventListener("click", function() {
+        playOneRound("paper", getComputerChoice());
+    });
+
+    scissors.addEventListener("click", function() {
+        playOneRound("scissors", getComputerChoice());
+    });
+}
+
+function playOneRound(userInput, computerInput){
+    let outcome; 
+    if (userInput === "rock"){
+        if (computerInput === "rock"){
+            console.log("It's a draw! The computer chose rock!")
+            outcome = "draw"
+        } else if (computerInput === "paper"){
+            console.log("You lose! The computer chose paper!")
+            outcome = "lose"
+        } else if (computerInput === "scissors"){
+            console.log("You win! The Computer chose rock!")
+            outcome = "win"
+        }
+    }
+
+    if (userInput === "paper"){
+        if (computerInput === "paper"){
+            console.log("It's a draw! The computer chose paper!")
+            outcome = "draw"
+        } else if (computerInput === "rock"){
+            console.log("You win! The computer chose rock!")
+            outcome = "win"
+        } else if (computerInput === "scissors"){
+            console.log("You lose! The Computer chose scissors!")
+            outcome = "lose"
+
+        }
+    }
+
+    if (userInput === "scissors"){
+        if (computerInput === "scissors"){
+            console.log("It's a draw! The computer chose scissors!")
+            outcome = "draw"
+        } else if (computerInput === "rock"){
+            console.log("You lose! The computer chose rock!")
+            outcome = "lose"
+        } else if (computerInput === "paper"){
+            console.log("You win! The Computer chose paper!")
+            outcome = "win"
+        }
+    }
+    updateScore(outcome)
+
+    return outcome
+}
+
+function updateScore(result) {
+    if (result === "win"){
+        wins += 1;
+    }else if (result==="lose"){
+        losses += 1;
+    } else if (result === "draw"){
+        draws += 1;
+    }
+
+    const counter = document.getElementById("counter");
+    counter.textContent = `Wins: ${wins} | Losses: ${losses} | Draws:${draws} `;
+    if (wins === 3 || losses === 3) {
+        gameOver();
+    }
+}
+
+
+function gameOver() {
+document.getElementById("rock").disabled = true;
+document.getElementById("paper").disabled = true;
+document.getElementById("scissors").disabled = true;
+
+if (wins > losses) {
+    counter.textContent = `You win! You won ${wins} rounds and the computer won ${losses} rounds`;
+} else if (losses > wins) {
+    counter.textContent = `You lose! You won ${wins} rounds and the computer won ${losses} rounds`;
+}
+}
+
+// This function initiates the game and only needs to be called once when the script runs
+function playGame(){
+    console.log("Let's play a best of three!");
+    playerSelection(); // This sets up the event listeners
+}
+
+function resetGame(){
+    wins = 0;
+    losses = 0;
+    draws = 0;
+
+    const counter = document.getElementById("counter");
+    counter.textContent = `Wins: ${wins} | Losses: ${losses} | Draws:${draws}`;
+
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+}
+
+playGame();
+
+document.getElementById("reset").addEventListener("click", function() {
+    resetGame()
+})
